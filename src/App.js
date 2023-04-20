a4bd7628e5e16f665a59f366ed7c5b6daa9c1673
shadab <sahdabansari972@gmail.com>
2023-04-20 20:44:23 +0530

    branch2

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Benifits of using React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ol>
+          <li>Component-based architecture</li>
+          <li>Virtual DOM for efficient updates</li>
+          <li>Rich ecosystem and community</li>
+          <li>Cross-platform development</li>
+          <li> Strong community support</li>
+        </ol>
+       
       </header>
+      <buttom btn = "App-button">Get started</buttom>
     </div>
   );
 }
