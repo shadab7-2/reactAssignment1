01a433dfc249f53e9917c5f1f14b2b325e0306f8
shadab <sahdabansari972@gmail.com>
2023-04-20 17:20:34 +0530

    push code

---
+++ src/components/ReactBenifits/ReactBenifits.js
@@ -0,0 +1,15 @@
+import React from 'react';
+  export default function ReactBenefits() {
+    return (
+      <div className="App-header">
+        <h2>Benefits of using React</h2>
+        <ol>
+          <li>Declarative programming model</li>
+          <li>Component-based architecture</li>
+          <li>Virtual DOM for efficient updates</li>
+          <li>Large community and ecosystem</li>
+          <li>Strong cummunity support</li>
+        </ol>
+      </div>
+    );
+  }
---
+++ src/components/ReactLogo/ReactLogo.js
@@ -0,0 +1,9 @@
+
+import logo from './logo.svg';
+ export default function ReactLogo() {
+  return (
+    <div className="logo">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
